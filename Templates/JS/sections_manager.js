2b22306f776b61c4e697b810f399e307a132c212
IOMOIO-hub <69336443+IOMOIO-hub@users.noreply.github.com>
2021-02-01 19:45:04 +0500

    feature

--- Templates/JS/sections_manager.js
+++ Templates/JS/sections_manager.js
@@ -6,3 +6,8 @@ function switch_to(section_id, btn) {
     document.getElementById(section_id).style.display = 'block';
     btn.className += ' active';
 }
+
+function reload(logo){
+    logo.style.animation = 'you_spin_me_right_round 1s';
+    setTimeout("location.reload()", 800);
+}
