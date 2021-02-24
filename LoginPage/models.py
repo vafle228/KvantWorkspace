9dd4df3cda697038d10d9e446618fa1fd7beb895
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2021-02-24 16:55:44 +0500

    Refactoring

--- LoginPage/models.py
+++ LoginPage/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 permission = (("Ученик", "Ученик"),
               ("Учитель", "Учитель"),
               ("Администратор", "Администратор")
