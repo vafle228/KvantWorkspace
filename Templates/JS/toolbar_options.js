4763d58083244cf2d9bc12d5e4c548cfce4a8b08
IOMOIO-hub <69336443+IOMOIO-hub@users.noreply.github.com>
2021-05-12 21:20:50 +0500

    toolbar

---
+++ Templates/JS/toolbar_options.js
@@ -0,0 +1,22 @@
+hljs.configure({
+    useBR: false
+    });
+var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+    [{ 'header': 1 }, { 'header': 2 }],
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'indent': '-1'}, { 'indent': '+1' }],
+    [{ 'color': [] }, { 'background': [] }],
+    [{ 'align': [] }],
+    ['formula']
+    ];
+var quill = new Quill('#editor', {
+  modules: {
+    syntax: true,
+    toolbar: toolbarOptions,
+  },
+  theme: 'snow',
+  spellcheck: 'true',
+  placeholder: 'Сообщение'
+});
