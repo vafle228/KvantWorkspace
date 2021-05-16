6af246788155c470a141eeaf92e051061e1ac1f9
IOMOIO-hub <69336443+IOMOIO-hub@users.noreply.github.com>
2021-05-16 13:41:00 +0500

    User searcher

---
+++ Templates/JS/draggable_options.js
@@ -0,0 +1,31 @@
+Draggable.create(".user",{
+    type:"x,y",
+    onRelease:dropItem,
+    autoScroll: 1
+    });
+
+    function dropItem(){
+        var boundsBefore, boundsAfter;
+        if (this.hitTest(".selected")){
+            boundsBefore = this.target.getBoundingClientRect();
+            $(this.target).appendTo('.selected');
+            boundsAfter = this.target.getBoundingClientRect();
+            TweenMax.fromTo(this.target, 0.2, {
+                x:"+=" + (boundsBefore.left - boundsAfter.left), 
+                y:"+=" + (boundsBefore.top - boundsAfter.top)
+            }, { x:0, y:0 });
+        } else {
+            if (this.hitTest(".unselected")){
+                boundsBefore = this.target.getBoundingClientRect();
+            $(this.target).appendTo('.unselected');
+            boundsAfter = this.target.getBoundingClientRect();
+            TweenMax.fromTo(this.target, 0.2, {
+                x:"+=" + (boundsBefore.left - boundsAfter.left), 
+                y:"+=" + (boundsBefore.top - boundsAfter.top)
+            }, { x:0, y:0 });
+            filterFunction($('.dropdown-input')[0]);
+            } else{
+                TweenMax.to(this.target,0.2,{x:0,y:0});
+            }
+        }
+    }
--- Templates/JS/sections_manager.js
+++ Templates/JS/sections_manager.js
@@ -1,23 +1,20 @@
-// Переход по вкладкам
-function switch_to(section_id, btn) {
-	for (let i = 0; i < 4; i++) {
-		$('body > section')[i].style.display = 'none';
-		$('aside button').removeClass('active');
-	}
-	document.getElementById(section_id).style.display = 'block';
-	btn.className += ' active';
-}
-
 // Перезагрузка старницы с анимацией логотипа
 function reload(logo) {
 	logo.style.animation = 'you_spin_me_right_round 1s';
 	setTimeout("location.reload()", 800);
 }
 
-// Закрытие формы
-function close_form(bg) {
-	bg.parentElement.style.display = 'none';
-	$("body").css("position", "relative");
+// Закрыть форму
+$(document).mouseup(function (e) {
+	var container = $("form");
+	if (container.has(e.target).length === 0) {
+		$(".form").hide();
+	}
+});
+
+// Открыть форму
+function open_form(form_class) {
+	$(form_class).show();
 }
 
 // Открытие формы по нажатию на урок
@@ -50,20 +47,42 @@ function open_create_mail_form() {
 	$("body").css("position", "fixed");
 }
 
-// Поиск получателя письма
+
+let filters = [];
+// Поиск пользователя
 function filterFunction(input) {
-	input.nextElementSibling.style.display = 'block';
-	var filter = input.value.toUpperCase();
-	var drops = input.nextElementSibling;
-	var a = drops.getElementsByTagName("a");
-	for (var i = 0; i < a.length; i++) {
-		txtValue = a[i].textContent || a[i].innerText;
-		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-			a[i].style.display = "block";
-		} else {
-			a[i].style.display = "none";
+	var substr = input.value.toUpperCase();
+	if (substr.trim()) {
+		$('.unselected').show();
+		var users = $('.unselected .user');
+		var counter = 0;
+		for (var i = 0; i < users.length; i++) {
+			users[i].style.display = 'none';
+
+			name_ = users[i].getElementsByTagName('h2')[0].textContent;
+			category_ = users[i].getElementsByTagName('h4')[0].textContent;
+			if (name_.toUpperCase().indexOf(substr) > -1 && filters.indexOf(category_) !== -1) {
+				users[i].style.display = 'flex';
+				counter += 1;
+			}
+			if (!counter) {
+				$('.unselected').hide();
+			}
 		}
+	} else {
+		$('.unselected').hide();
 	}
+}
+
+function filter_applying(button, filter) {
+	if ($(button).hasClass('active')) {
+		$(button).removeClass('active');
+		filters.splice(filters.indexOf(filter), 1);
+	} else {
+		$(button).addClass('active');
+		filters.push(filter);
+	}
+	filterFunction($('.dropdown-input')[0]);
 }
 
 // Выбор получателя письма
@@ -74,8 +93,8 @@ function search_fill(a) {
 }
 
 // Сменить тему
-function switch_theme(){
-	if($('body').hasClass('light__theme')){
+function switch_theme() {
+	if ($('body').hasClass('light__theme')) {
 		$('.light__theme').toggleClass('light__theme dark__theme');
 	} else {
 		$('.dark__theme').toggleClass('dark__theme light__theme');
@@ -83,8 +102,8 @@ function switch_theme(){
 }
 
 // Сменить цветовую гамму
-function switch_color_scheme(){
-	if($('body').hasClass('blue__color__scheme')){
+function switch_color_scheme() {
+	if ($('body').hasClass('blue__color__scheme')) {
 		$('.blue__color__scheme').toggleClass('blue__color__scheme orange__color__scheme');
 	} else {
 		$('.orange__color__scheme').toggleClass('orange__color__scheme blue__color__scheme');
