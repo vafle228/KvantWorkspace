82d9be010e6139c89da7c56b49ac6d480384aed7
IOMOIO-hub <69336443+IOMOIO-hub@users.noreply.github.com>
2022-02-21 21:15:15 +0500

    cringe push

---
+++ Templates/JS/toolbar_options.js
@@ -0,0 +1,38 @@
+hljs.configure({
+    useBR: false
+    });
+var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+    [{ 'header': 1 }, { 'header': 2 }],
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'indent': '-1'}, { 'indent': '+1' }],
+    [{ 'color': [] }, { 'background': [] }],
+    // [{ 'align': [] }],
+    ['formula'],
+    ['link']
+    ];
+var quill = new Quill('#editor', {
+  modules: {
+    syntax: true,
+    toolbar: toolbarOptions,
+  },
+  theme: 'snow',
+  spellcheck: true
+});
+var quill = new Quill('#editor1', {
+  modules: {
+    syntax: true,
+    toolbar: toolbarOptions,
+  },
+  theme: 'snow',
+  spellcheck: true
+});
+var quill = new Quill('#editor2', {
+  modules: {
+    syntax: true,
+    toolbar: toolbarOptions,
+  },
+  theme: 'snow',
+  spellcheck: true
+});
