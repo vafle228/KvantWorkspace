baef53b5bb7a0a4b70c9512d8f3e24b3cb14ef2a
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2022-03-24 17:47:02 +0500

    Yes

--- src/JournalApp/services/access.py
+++ src/JournalApp/services/access.py
@@ -46,7 +46,7 @@ class KvantBaseAccessMixin(KvantObjectExistsMixin):
     
     def accessTest(self, **kwargs):
         if super().accessTest(**kwargs):
-            base = getBaseById(id=kwargs.get(self.request_object_arg))
+            base = getBaseById(kwargs.get(self.request_object_arg))
             return self._teacherAccessTest(kwargs.get('user'), base)
         return False
 
