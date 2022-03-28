4df6f7b127521f1c0fe030fd0ef8592b54ed874a
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2022-03-28 21:57:21 +0500

    Host upd

--- src/Config/wsgi.py
+++ src/Config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KvantWorkspace.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Config.settings')
 
 application = get_wsgi_application()
